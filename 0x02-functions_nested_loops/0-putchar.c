7c5605dc6e9f7beefae040409e90cb9c6c482ad3
bakry411 <you@example.com>
2022-09-16 14:50:21 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ *
+ *
+ */
+	int main()
+{ 
+        printf("_putchar");
+	printf("\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/*
+ *
+ *
+*/
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch < = 'z')
+{
+	_putchar(ch);
+	ch++;
+}
+	_putchar("\n");
+	return;
+}
